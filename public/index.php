696707590a2029faaf7c45a7535003d9f18a3f95
dominic baeten <d228528@edu.rocwb.nl>
2018-04-19 11:15:11 +0200

    Test

--- public/index.php
+++ public/index.php
@@ -8,3 +8,17 @@
     </div>
 
 <?php require(realpath(__DIR__) . '/templates/footer.php');
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>GERBEN!!!</h1>
+</body>
+</html>
