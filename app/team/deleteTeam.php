c8bd96515ee79be8ceb56f6325414ec09b254ece
dominic baeten <d228528@edu.rocwb.nl>
2018-05-14 09:23:10 +0200

    add team delete script

--- app/team/deleteTeam.php
+++ app/team/deleteTeam.php
@@ -1,1 +1,17 @@
 <?php
+
+require ('../dbconn.php');
+
+if ($_SERVER['REQUEST_METHOD'] == "GET")
+{
+    if (isset($_GET["teamId"]))
+    {
+        $teamId = $_GET["teamId"];
+
+        $dbc = $conn;
+        $sql = "DELETE FROM `tbl_teams` WHERE `id` = '$teamId'";
+        $dbc->query($sql);
+
+        header('Location: ' . '../../public/player.php?del=success');
+    }
+}
--- public/team.php
+++ public/team.php
@@ -1,1 +1,36 @@
 <?php
+require ('../app/dbconn.php');
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+if (isset($_GET['add']) && $_GET['add'] == "failed"){
+    echo '<script>';
+    echo 'alert("Failed to add team");';
+    echo 'window.location.href = team.php";';
+    echo '</script>';
+}
+else if (isset($_GET['add']) && $_GET['add'] == "success"){
+    echo '<script >';
+    echo 'alert("Succesfully added a team");';
+    echo 'window.location.href = "team.php";';
+    echo '</script>';
+}
+else if (isset($_GET['del']) && $_GET['del'] == "success"){
+    echo '<script >';
+    echo 'alert("Succesfully deleted team");';
+    echo 'window.location.href = "team.php";';
+    echo '</script>';
+}
+?>
+</body>
+</html>
